b8e96ceecff59f631d780869631c2f11dee7478b
Arthur Wambst <wambst.arthur2@gmail.com>
2024-02-12 21:10:13 +0100

    save

--- minecraftEggs/entrypoint.sh
+++ minecraftEggs/entrypoint.sh
@@ -4,4 +4,4 @@ cd /home/container
 # Output Current Java Version
 java -version ## only really needed to show what version is being used. Should be changed for different applications
 
-curl -OJ https://meta.fabricmc.net/v2/versions/loader/1.20.4/0.15.6/1.0.0/server/jar
+java -Xmx2G -jar fabric-server-mc.1.20.4-loader.0.15.6-launcher.1.0.0.jar nogui
