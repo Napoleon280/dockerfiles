9e65b15adbad08a38322f1594adc1a1d4dbf4bbf
Arthur Wambst <127991853+Napoleon280@users.noreply.github.com>
2024-02-13 19:25:24 +0100

    Update entrypoint.sh

--- minecraftEggs/entrypoint.sh
+++ minecraftEggs/entrypoint.sh
@@ -4,4 +4,7 @@ cd /home/container
 # Output Current Java Version
 java -version ## only really needed to show what version is being used. Should be changed for different applications
 
-${STARTUP}
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+${MODIFIED_STARTUP}
