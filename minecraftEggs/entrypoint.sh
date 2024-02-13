0789cf560289cffc1bcd72ed25ab93bfbeb6fc32
Arthur Wambst <127991853+Napoleon280@users.noreply.github.com>
2024-02-13 19:15:49 +0100

    Update entrypoint.sh

--- minecraftEggs/entrypoint.sh
+++ minecraftEggs/entrypoint.sh
@@ -4,4 +4,3 @@ cd /home/container
 # Output Current Java Version
 java -version ## only really needed to show what version is being used. Should be changed for different applications
 
-java -Xmx2G -jar fabric-server-mc.1.20.4-loader.0.15.6-launcher.1.0.0.jar nogui
